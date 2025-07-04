d7a4d3c00a11fc688f2286b83b0a11a8f5cbd0c0
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2025-07-04 17:27:12 +0530

    Added VT support

--- src/main/java/com/example/retail/config/VirtualThreadConfig.java
+++ src/main/java/com/example/retail/config/VirtualThreadConfig.java
@@ -1,6 +1,7 @@
 package com.example.retail.config;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -11,9 +12,15 @@ import java.util.concurrent.Executors;
 @Configuration
 public class VirtualThreadConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(VirtualThreadConfig.class);
+
     @ConditionalOnProperty(name = "spring.threads.virtual.enabled", havingValue = "true")
     @Bean
     public TomcatProtocolHandlerCustomizer<?> tomcatExecutorCustomizer() {
-        return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        logger.info("Virtual threads enabled - configuring Tomcat to use virtual thread executor");
+        return protocolHandler -> {
+            logger.debug("Setting virtual thread executor for protocol handler: {}", protocolHandler.getClass().getSimpleName());
+            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        };
     }
 }
